9dcd6006f9841fb790d9b6bd4b3ecea54962cde6
E-Ozdemir <emreclarusway@gmail.com>
2021-08-09 20:11:35 +0200

    end of the class components

--- Lesson04-React-Events/react-events/src/components/eventBinding/EventBind.js
+++ Lesson04-React-Events/react-events/src/components/eventBinding/EventBind.js
@@ -1,10 +1,51 @@
+import { Component } from "react";
+
+class EventBind extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      message: "Hello"
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
   /**
    * TODO List
    * create class component
    * 1. Binding in render method
    * 2. Binding in render method using arrow function
    * 3. Binding in the constructor
-   * 4. Class property as an arrow function
+   * 4. Class property as an arrow functionhcd 
+   * 5.set argument to event handler
    */
 
+  // handleClick(){
+  //   this.setState({message: "Goodbye!"})
+  //   console.log("this: ", this);
+  // }
+  // [1,2,3]
+  // handleClick =()=>{
+  //   this.setState({message: "Goodbye!"})
+  //   console.log("this: ", this);
+  // }
+  // [4]
 
+  handleClick(name){
+    this.setState({message: "Goodbye! ${name}"})
+    console.log("this: ", this);
+  }
+ 
+
+  render(){
+    return(
+      <div>
+        <p>{this.state.message}</p>
+        {/* <button onClick={this.handleClick.bind(this)}>click</button> */}
+        {/* <button onClick={() => this.handleClick()}>click</button> */}
+        {/* <button onClick={this.handleClick}>click</button> */}
+        <button onClick={() => this.handleClick("Roy")}>click</button>
+      </div>
+    )
+  }
+}
+
+export default EventBind;
