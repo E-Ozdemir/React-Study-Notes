b58337e0dbf506a09786b0a3034b53d8b92e0b3b
E-Ozdemir <emreclarusway@gmail.com>
2021-08-09 18:41:22 +0200

    card display

--- Lesson02-React_JSX/lesson-jsx/src/App.js
+++ Lesson02-React_JSX/lesson-jsx/src/App.js
@@ -1,14 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import ClassComponent from './components/ClassComponent';
-import FunctionalComponent from './components/FunctionalComponent';
+// import ClassComponent from './components/ClassComponent';
+// import FunctionalComponent from './components/FunctionalComponent';
+// import Card from './components/Card';
+import Card2 from './components/Card2';
 
 function App() {
+  const motto = "Seni yenecegim react!!!!";
   return (
+
+    //props----
     <div className="App">
-      <ClassComponent name="Ege"/>
-      <FunctionalComponent name="Ela"/>
+      <Card2 name="Cody Pines" job="React Inst."/>
+      <Card2 name="Emre Özdemir" job="Aviation Inst."/>
+      <Card2 name="Efe Özdemir" job="React Inst."/>
     </div>
+
+
+
+    //------------------
+    // <div className="App">
+    //   <h1>App Component</h1>
+    //   <ClassComponent/>
+    //   <FunctionalComponent/>
+    //   {/* <ClassComponent name="Ege"/> */}
+    //   {/* <FunctionalComponent name="Ela"/> */}
+    // </div>
   );
 }
 export default App;
