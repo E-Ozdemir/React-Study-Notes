b4e8fbde170869d7661f4e42d0e7bf8cd54a36e6
E-Ozdemir <emreclarusway@gmail.com>
2021-09-01 22:14:10 +0200

    some commits

---
+++ ReactHook/hook02/src/FunctionalComponent.js
@@ -0,0 +1,49 @@
+import { useState, useEffect, useRef } from "react";
+const FunctionalComponent = () => {
+  const inputRef = useRef(null);
+  // const divRef = useRef(null);
+  const value = useRef(0);
+  console.log('value', value)
+  const [counter, setCounter] = useState(0);
+  const [text, setText] = useState("");
+  useEffect(() => {
+    console.log("useEffect");
+    // return () => {console.log("func. comp. unmount")}
+  },[counter])
+  console.log("Func Comp Rendered!")
+  const increase = () => {
+    setCounter(counter + 1);
+    value.current = value.current + 1;
+  };
+  const changeBGColor = () => {
+    // console.log(inputRef.current.value);
+    // const newInputTwo = document.querySelector("input");
+    // console.log(newInputTwo);
+    // newInputTwo.style.backgroundColor = text;
+    inputRef.current.parentElement.style.backgroundColor = text;
+  };
+  const handleText = (e) => {
+    setText(e.target.value);
+  };
+// const onDivBorder = () => {
+//   divRef.current.style.border = "10px solid black"
+// }
+  return (
+    <div className="function">
+      <h2>Functional Component</h2>
+      <p>Counter: {counter}</p>
+      <button onClick={() => increase()}>Increase</button>
+      <hr />
+      <input
+        id="new"
+        ref={inputRef}
+        type="text"
+        value={text}
+        onChange={handleText}
+      />
+      <button onClick={() => changeBGColor()}>BG Color</button>
+      {/* <button onClick={() => onDivBorder()}>BORDER</button> */}
+    </div>
+  );
+};
+export default FunctionalComponent;
