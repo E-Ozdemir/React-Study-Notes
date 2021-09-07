c45a0494d545b9f7953d0df808c437d984fb64de
E-Ozdemir <emreclarusway@gmail.com>
2021-09-07 19:38:15 +0200

    Breaking Bad Characters with api fetch

---
+++ Api_Lesson/api_lesson/src/App.js
@@ -0,0 +1,29 @@
+import {useState,useEffect} from 'react'
+import Card from './components/Card'
+// import data from './data.json'
+
+function App(){
+  const [itemList,setitemList] =useState([]);
+
+  useEffect(() => {
+    fetch('https://breakingbadapi.com/api/characters')
+    .then(response => response.json())
+    .then(data=>setitemList(data));
+
+  }, [])
+
+  return(
+    <div className='App'>
+      <header className='header'>
+        <img src="https://www.logolynx.com/images/logolynx/55/55a537ca9549600cf7d1d0b274b3bc61.png" alt="Breaking Bad Logo" />
+      </header>
+      <div className="cards">
+        {itemList.map((item)=>(
+          <Card key={item.char_id} item={item}/>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
